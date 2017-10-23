4b98fd58b2bdb25fe8abd28fd830b2d2aea33dc0
Da Hao <v@daidahao.me>
2017-10-23 19:51:57 +0800

    add Main and Chat Activity

---
+++ app/src/main/java/sustech/unknown/channelx/ChatActivity.java
@@ -0,0 +1,53 @@
+package sustech.unknown.channelx;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import co.intentservice.chatui.ChatView;
+import co.intentservice.chatui.models.ChatMessage;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        Intent intent = getIntent();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
+        toolbarTitle.setText(intent.getStringExtra(MainActivity.CHANNEL_MESSAGE));
+
+        ChatView chatView = (ChatView) findViewById(R.id.chat_view);
+        chatView.addMessage(new ChatMessage(
+                "Message received", System.currentTimeMillis(), ChatMessage.Type.RECEIVED));
+
+        chatView.setOnSentMessageListener(new ChatView.OnSentMessageListener() {
+            @Override
+            public boolean sendMessage(ChatMessage chatMessage) {
+                return true;
+            }
+        });
+
+        chatView.setTypingListener(new ChatView.TypingListener() {
+            @Override
+            public void userStartedTyping() {
+
+            }
+
+            @Override
+            public void userStoppedTyping() {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/sustech/unknown/channelx/MainActivity.java
@@ -0,0 +1,27 @@
+package sustech.unknown.channelx;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "sustech.unknown.channelx.chat.EXTRA";
+    public static final String CHANNEL_MESSAGE = "sustech.unknown.channelx.chat.CHANNEL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, ChatActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(CHANNEL_MESSAGE, "CHANNEL 1");
+        startActivity(intent);
+    }
+}
