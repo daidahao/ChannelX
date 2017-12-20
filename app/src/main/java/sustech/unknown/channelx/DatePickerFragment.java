7f197a1d1375e32144d983b344626eb5f0584b40
Da Hao <v@daidahao.me>
2017-12-20 13:07:32 +0800

    when creating channel, cannot pick a date previous to today

--- app/src/main/java/sustech/unknown/channelx/DatePickerFragment.java
+++ app/src/main/java/sustech/unknown/channelx/DatePickerFragment.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -16,22 +17,18 @@ import java.util.Calendar;
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
 
     public static String dateTextKey = "dateTextKey";
-
-
+    private int year, month, day;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
         final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
 //        int year = 2018;
 //        int month = 0;
 //        int day = 1;
-
-
-
         // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
@@ -39,6 +36,12 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         EditText dateText = getActivity().findViewById(R.id.dateText);
-        dateText.setText(year + "-" + (month + 1) + "-" + dayOfMonth);
+
+        if ((year < this.year) || (year == this.year && month < this.month) ||
+                (year == this.year && month == this.month && dayOfMonth < this.day)) {
+            dateText.setText(this.year + "-" + (this.month + 1) + "-" + this.day);
+        } else {
+            dateText.setText(year + "-" + (month + 1) + "-" + dayOfMonth);
+        }
     }
 }
