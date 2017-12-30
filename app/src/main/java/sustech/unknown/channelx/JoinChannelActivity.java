1608b4e95fdcd3b5c9bf027a73bb2c21cbffa845
ShenChuanFu <595311942@qq.com>
2017-12-30 22:19:54 +0800

    加入二维码的扫描和自动加入channel

--- app/src/main/java/sustech/unknown/channelx/JoinChannelActivity.java
+++ app/src/main/java/sustech/unknown/channelx/JoinChannelActivity.java
@@ -7,9 +7,14 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import io.github.xudaojie.qrcodelib.CaptureActivity;
 import sustech.unknown.channelx.command.JoinChannelOnFailureMessageCommand;
 import sustech.unknown.channelx.command.JoinChannelOnSuccessMessageCommand;
 import sustech.unknown.channelx.dao.ChannelDao;
@@ -18,16 +23,27 @@ import sustech.unknown.channelx.util.ToastUtil;
 
 public class JoinChannelActivity extends AppCompatActivity {
 
+    private static final int REQUEST_QR_CODE = 1;
     private EditText editText;
     private String channelId;
-
+    private SurfaceView mySurfaceView;
+    //private QREader qrEader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_channel);
 
+
+
         initializeToolbar();
         initializeEditText();
+        // Setup SurfaceView
+        // -----------------
+
+
+        // Init QREader
+        // ------------
+
     }
 
     private void initializeEditText() {
@@ -41,8 +57,33 @@ public class JoinChannelActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
     public void onJoinChannel(View view) {
-       checkChannelExists(editText.getText().toString());
+//        Intent intent = new Intent(this, DecoderActivity.class);
+//        startActivityForResult(intent, 999);
+
+        checkChannelExists(editText.getText().toString());
+    }
+
+    public void onJoinChannelByScan(View view) {
+        Intent i = new Intent(this, CaptureActivity.class);
+       // i.putExtra("channelID", "");
+        this.startActivityForResult(i, REQUEST_QR_CODE);
+
+        //checkChannelExists(editText.getText().toString());
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK
+                && requestCode == REQUEST_QR_CODE
+                && data != null) {
+            String result = data.getStringExtra("result");
+            editText.setText(result);
+            checkChannelExists(editText.getText().toString());
+            //Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+
+        }
     }
 
     private void checkChannelExists(String channelId) {
