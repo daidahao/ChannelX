9e0470e204f1cd687c522980b6ea2f08d4eb748e
ShenChuanFu <595311942@qq.com>
2017-12-31 10:26:26 +0800

    修改错误
    添加string memberlist

--- app/src/main/java/sustech/unknown/channelx/QRActivity.java
+++ app/src/main/java/sustech/unknown/channelx/QRActivity.java
@@ -42,7 +42,7 @@ public class QRActivity extends AppCompatActivity {
                 ImageView imageView = findViewById(R.id.imageView2);
                 QRCodeWriter writer = new QRCodeWriter();
 
-                imageView.setImageBitmap(QRCode.from(meCardContent).bitmap());
+               // imageView.setImageBitmap(QRCode.from(meCardContent).bitmap());
 
             }
         });
@@ -63,53 +63,5 @@ public class QRActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    public static void getQrcodeImage(String content,String imagePath){
-        int width = 235;
-        int height = 235;
-        //实例化一个对象
-        QRCode qrcode = new  QRCode();
-        //编码方式
 
-        qrcode.setQrcodeEncodeMode('B');
-        //二维码的版本
-        qrcode.setQrcodeVersion(15);
-        //排错率
-        qrcode.setQrcodeErrorCorrect('M');
-
-        //创建一个图板
-        BufferedImage image = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
-        //画笔
-        Graphics2D gs = image.createGraphics();
-        //设置二维码的背景颜色
-        gs.setBackground(Color.white);
-        gs.setColor(Color.black);
-        gs.clearRect(0, 0, width, height);
-
-        byte[] codeOut = null;
-        try{
-            codeOut = content.getBytes("utf-8");
-            boolean[][] code = qrcode.calQrcode(codeOut);
-            for(int i=0;i<code.length;i++){
-                for(int j=0;j<code.length;j++){
-                    if(code[j][i]){
-                        gs.fillRect(j*3+2, i*3+2, 3, 3);
-                    }
-                }
-            }
-            //为二维码中间镶嵌入的小照片。
-            File file = new File("C:\\Users\\tangjinhui\\Desktop\\image.png");
-            Image srcImage = ImageIO.read(file);
-            int width2 = srcImage.getWidth(null);
-            int height2 = srcImage.getHeight(null);
-            gs.drawImage(srcImage, 83, 83, width2, height2,null);
-            //释放资源
-            gs.dispose();
-            image.flush();
-            //写入制定的路径
-            ImageIO.write(image, "png",new File(imagePath));
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-    }
 }
